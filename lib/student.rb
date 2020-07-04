f2b0de64459a26d3b7806ef1a541aaf2d1cb418b
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-04 07:21:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,6 +47,14 @@ class Student
     new_student
   end
 
+  def self.new_from_db(row)
+    new_student = Student.new 
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+    
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
