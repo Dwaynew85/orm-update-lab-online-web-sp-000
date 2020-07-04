20cc3812c132fb4026b45bd7a5629ee88d4c6adb
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-07-04 07:40:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,9 +31,9 @@ class Student
   end
 
   def save
-    if self.id 
-      self.update 
-    else      
+    if self.id
+      self.update
+    else
       sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
